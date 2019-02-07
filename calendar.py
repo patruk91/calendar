b74bfb19c963229416a56a8f89b71b82a63f8b33
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-07 09:18:03 +0100

    Display menu with options in upi, add main function in calendar

---
+++ calendar.py
@@ -0,0 +1,9 @@
+import upi
+
+
+def main():
+    upi.display_menu()
+
+
+if __name__ == '__main__':
+    main()
--- upi.py
+++ upi.py
@@ -1,5 +1,12 @@
 def display_menu():
+    """
+    Display options for an organise meeting.
+    :return: user choice: organise new meeting, cancel, or quit from program
+    """
     print("Menu:")
-    print("(s) schedule a new meeting"
-          "(c) cancel an existing meeting"
-          "")
+    print("\n(s) schedule a new meeting"
+          "\n(c) cancel an existing meeting"
+          "\n(q) quit")
+
+    user_choice = input("Your choice: ")
+    return user_choice
